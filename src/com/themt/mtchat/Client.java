32e79b862ade98f776e7d2e6094fb06831e82b7b
tin <minhtien0601200@gmail.com>
2024-05-15 22:53:19 +0700

    Update Main Client Window

--- src/com/themt/mtchat/Client.java
+++ src/com/themt/mtchat/Client.java
@@ -8,7 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridBagLayout;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+
 import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.Font;
 
 public class Client extends JFrame {
 
@@ -17,6 +25,7 @@ public class Client extends JFrame {
 	
 	private String name, address;
 	private int port;
+	private JTextField txtMessage;
 
 	public Client(String name, String address, int port) {
 		setTitle("MT Chat Client");
@@ -27,6 +36,12 @@ public class Client extends JFrame {
 	}
 	
 	private void createWindow() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(880, 550);
 		setLocationRelativeTo(null);
@@ -36,20 +51,47 @@ public class Client extends JFrame {
 		setContentPane(contentPane);
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{16, 857, 7}; // SUM = 880
+		gbl_contentPane.columnWidths = new int[]{28, 815, 30, 7}; // SUM = 880
 		gbl_contentPane.rowHeights = new int[]{35, 475, 40}; //SUM = 550
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
 		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JTextArea txtrHistory = new JTextArea();
+		txtrHistory.setEditable(false);
 		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.insets = new Insets(0, 0, 5, 5);
 		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
 		gbc_txtrHistory.gridx = 1;
 		gbc_txtrHistory.gridy = 1;
+		gbc_txtrHistory.gridwidth = 2;
+		gbc_txtrHistory.insets = new Insets(0, 5, 0, 0);
 		contentPane.add(txtrHistory, gbc_txtrHistory);
 		
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		
+		txtMessage = new JTextField();
+		txtMessage.setFont(new Font("Arial", Font.PLAIN, 13));
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 1;
+		gbc_txtMessage.gridy = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 2;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+		
+		requestFocus();
+		txtMessage.requestFocus();
 		setVisible(true);
 	}
 
