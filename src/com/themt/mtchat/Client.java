423cac031fff9a18872a62cddf60bcf097fac82a
tin <minhtien0601200@gmail.com>
2024-05-16 00:18:21 +0700

    Action and KeyListener

--- src/com/themt/mtchat/Client.java
+++ src/com/themt/mtchat/Client.java
@@ -17,6 +17,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Client extends JFrame {
 
@@ -70,14 +72,22 @@ public class Client extends JFrame {
 		gbc_txtrHistory.insets = new Insets(0, 5, 0, 0);
 		contentPane.add(txtrHistory, gbc_txtrHistory);
 		
-		JButton btnNewButton = new JButton("Send");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnSend = new JButton("Send");
+		btnSend.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				send(txtMessage.getText());
 			}
 		});
 		
 		txtMessage = new JTextField();
+		txtMessage.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					send(txtMessage.getText());
+				}
+			}
+		});
 		txtMessage.setFont(new Font("Arial", Font.PLAIN, 13));
 		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
 		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
@@ -86,17 +96,23 @@ public class Client extends JFrame {
 		gbc_txtMessage.gridy = 2;
 		contentPane.add(txtMessage, gbc_txtMessage);
 		txtMessage.setColumns(10);
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 2;
-		gbc_btnNewButton.gridy = 2;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
 		
 		requestFocus();
 		txtMessage.requestFocus();
 		setVisible(true);
 		
 		txtMessage.requestFocusInWindow();
+	}
+	private void send(String message) {
+		if(message.equals("")) return;
+		message = name + ": " + message;
+		console(message);
+		txtMessage.setText("");
 	}
 	
 	public void console(String message) {
