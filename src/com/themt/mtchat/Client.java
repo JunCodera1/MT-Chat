d64f193f8f68120cf053cdcacfd82ed7cf684e36
tin <minhtien0601200@gmail.com>
2024-05-20 09:58:56 +0700

    Send packet to all clients

--- src/com/themt/mtchat/Client.java
+++ src/com/themt/mtchat/Client.java
@@ -179,6 +179,7 @@ public class Client extends JFrame {
 		if(message.equals("")) return;
 		message = name + ": " + message;
 		console(message);
+		message = "/m/" + message;
 		send(message.getBytes());
 		txtMessage.setText("");
 	}
--- src/com/themt/mtchat/server/Server.java
+++ src/com/themt/mtchat/server/Server.java
@@ -3,10 +3,10 @@ package com.themt.mtchat.server;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class Server implements Runnable{
 	
@@ -73,6 +73,28 @@ public class Server implements Runnable{
 		receive.start();
 	}
 	
+	private void sendToAll(String message) {
+		for(int i = 0; i < clients.size(); i++){
+			ServerClient client = clients.get(i);
+			send(message.getBytes(), client.address, client.port);
+		}
+	}
+	
+	private void send(final byte[] data, final InetAddress address, int port) {
+		send = new Thread("Send") {
+			public void run() {
+				DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+				try {
+					socket.send(packet);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		send.start();	
+	}
+	
+	
 	private void process(DatagramPacket packet) {
 	    String string = new String(packet.getData());
 	    if(string.startsWith("/c/")) {
@@ -81,7 +103,11 @@ public class Server implements Runnable{
 	    	System.out.println("Identifier: " + id);
 			clients.add(new ServerClient(string.substring(3, string.length()), packet.getAddress(), packet.getPort(), id));
 			System.out.println(string.substring(3, string.length()));
-		} else {
+		} 
+	    else if(string.startsWith("/m/")){
+			sendToAll(string);
+		}
+		else {
 			System.out.println(string);
 		}
 		
