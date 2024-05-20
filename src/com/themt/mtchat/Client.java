cae12155ac3860aad3308b82e24939ed66473268
tin <minhtien0601200@gmail.com>
2024-05-20 10:15:31 +0700

    Interface and Networking Separation

--- src/com/themt/mtchat/Client.java
+++ src/com/themt/mtchat/Client.java
@@ -25,7 +25,7 @@ import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.DefaultCaret;
 
-public class Client extends JFrame {
+public class Client{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -36,30 +36,14 @@ public class Client extends JFrame {
 	private InetAddress ip;
 	private Thread send;
 	
-	private JPanel contentPane;
-	private JTextField txtMessage;
-	private JTextArea history;
-	private DefaultCaret caret;
-	
-
 	public Client(String name, String address, int port) {
-		setTitle("MT Chat Client");
 		this.name = name;
-		this.port = port;
 		this.address = address;
-		createWindow();
-		boolean connect = openConnection(address);
-		if(!connect) {
-			System.err.println("Connection failed!");
-			console("Connection failed!");
-		}
-		createWindow();
-		console("Attempting a connection to " + address + ":" + port + ", user: " + name);
-		String connection = "/c/" + name;
-		send(connection.getBytes());
+		this.port = port;
 	}
 	
-	private boolean openConnection(String address) {
+	
+	public boolean openConnection(String address) {
 		try {
 			socket = new DatagramSocket();
 			ip = InetAddress.getByName(address);
@@ -71,6 +55,18 @@ public class Client extends JFrame {
 			return false;
 		}
 		return true;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public int getPort() {
+		return port;
 	}
 	
 	private String receive() {
@@ -87,7 +83,7 @@ public class Client extends JFrame {
 		return message;
 	}
 	
-	private void send(final byte[] data) {
+	public void send(final byte[] data) {
 		send = new Thread("Send") {
 			public void run() {
 				DatagramPacket packet = new DatagramPacket(data, data.length, ip, port);
@@ -101,91 +97,6 @@ public class Client extends JFrame {
 		send.start();
 	}
 	
-	private void createWindow() {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(880, 550);
-		setLocationRelativeTo(null);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
-		setContentPane(contentPane);
-		
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{28, 815, 30, 7}; // SUM = 880
-		gbl_contentPane.rowHeights = new int[]{35, 475, 40}; //SUM = 550
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
-		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
-		
-		history = new JTextArea();
-		history.setEditable(false);
-		JScrollPane scroll = new JScrollPane(history);
-		caret = (DefaultCaret) history.getCaret();
-		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		GridBagConstraints scrollConstraints = new GridBagConstraints();
-		scrollConstraints.insets = new Insets(0, 0, 5, 5);
-		scrollConstraints.fill = GridBagConstraints.BOTH;
-		scrollConstraints.gridx = 0;
-		scrollConstraints.gridy = 0;
-		scrollConstraints.gridwidth = 3;
-		scrollConstraints.gridheight = 2;
-		scrollConstraints.insets = new Insets(0, 5, 0, 0);
-		contentPane.add(scroll, scrollConstraints);
-		
-		JButton btnSend = new JButton("Send");
-		btnSend.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				send(txtMessage.getText());
-			}
-		});
-		
-		txtMessage = new JTextField();
-		txtMessage.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-					send(txtMessage.getText());
-				}
-			}
-		});
-		txtMessage.setFont(new Font("Arial", Font.PLAIN, 13));
-		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
-		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
-		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtMessage.gridx = 0;
-		gbc_txtMessage.gridy = 2;
-		gbc_txtMessage.gridwidth = 2;
-		contentPane.add(txtMessage, gbc_txtMessage);
-		txtMessage.setColumns(10);
-		GridBagConstraints gbc_btnSend = new GridBagConstraints();
-		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
-		gbc_btnSend.gridx = 2;
-		gbc_btnSend.gridy = 2;
-		contentPane.add(btnSend, gbc_btnSend);
-		
-		requestFocus();
-		txtMessage.requestFocus();
-		setVisible(true);
-		
-		txtMessage.requestFocusInWindow();
-	}
-	private void send(String message) {
-		if(message.equals("")) return;
-		message = name + ": " + message;
-		console(message);
-		message = "/m/" + message;
-		send(message.getBytes());
-		txtMessage.setText("");
-	}
 	
-	public void console(String message) {
-		history.append(message + "\n\r");
-	}
 
 }
